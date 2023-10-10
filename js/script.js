99652e602185b592ce523ba42bcf8bfaad2e0afa
Eggward12 <cristian.sanchez.casd@gmail.com>
2023-10-10 01:11:57 -0500

    ajuste margen de error

--- js/script.js
+++ js/script.js
@@ -103,14 +103,14 @@ function compararPos(id, posPieza, cuadros) {
   // console.log(cuadros)
   //funcion para comparar.
   if (
-    posPieza.left >= cuadros[id].position.left - 10 &&
-    posPieza.left <= cuadros[id].position.left + 10 &&
-    posPieza.top >= cuadros[id].position.top - 10 &&
-    posPieza.top <= cuadros[id].position.top + 10 &&
-    posPieza.right >= cuadros[id].position.right - 10 &&
-    posPieza.right <= cuadros[id].position.right + 10 &&
-    posPieza.bottom >= cuadros[id].position.bottom - 10 &&
-    posPieza.bottom <= cuadros[id].position.bottom + 10 
+    posPieza.left >= cuadros[id].position.left - 5 &&
+    posPieza.left <= cuadros[id].position.left + 5 &&
+    posPieza.top >= cuadros[id].position.top - 5 &&
+    posPieza.top <= cuadros[id].position.top + 5 &&
+    posPieza.right >= cuadros[id].position.right - 5 &&
+    posPieza.right <= cuadros[id].position.right + 5 &&
+    posPieza.bottom >= cuadros[id].position.bottom - 5 &&
+    posPieza.bottom <= cuadros[id].position.bottom + 5 
   ) {
     piezasTot.forEach((e, i) => {
       // if (e.id.split("-")[1] === id) {
